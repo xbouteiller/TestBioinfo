b8c254af290a33aa7ef76ba7bb42d3bfa13e5905
Xavier Bouteiller <bouteiller.xavier@gmail.com>
2020-02-12 10:58:38 +0100

    script for populating sqllite db v2

--- main_script.py
+++ main_script.py
@@ -5,33 +5,57 @@ Created on Tue Feb 11 14:57:18 2020
 @author: Xavier Bouteiller
 @email: bouteiller.xavier@gmail.com
 
+This program contains 4 main steps:
+    1. download .gz .gz.tbi .md5 files from the http://www.nlgenome.nl/ for a specific chromosome defined by the user    
+    2. load & extract values from vcf
+    3. instantiate a sqllite db
+    4. populate the db
+    5. test the created db
+
+Need to define the working folder as input (should contains the bioinfo folder with the __init__.py)
 
 """
 
-import os
-os.chdir('/home/xav/Documents/test_python')
+# %% Dependencies 
 
-
-#function for downloading files
-from bioinfo.downloadFiles import get_file_links, download_files_series
+# Generic import
 import vcf
-import sqlalchemy
+import os
+# Import Table, Column, String, and Integer
+from sqlalchemy import Table, Column, String, Integer, Float
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy import insert
+from sqlalchemy import select, and_
+import pandas as pd
 
+# specific import
+# Enter the path to working folder (should contains bioinfo folder with the __init__.py)
+path = input("Enter path to working folder (should contains bioinfo folder with the __init__.py): ") or '/home/xav/Documents/test_python'
+print('path is: {}'.format(path))
 
+# change the path to the folder where the bioinfo folder (with __init__.py)  is located
+os.chdir(path) 
+# user defined functions : functions for downloading file
+from bioinfo.downloadFiles import get_file_links, download_files_series
 
-#%% Download useful files
+#%% step 1: Download useful files
 url = "https://molgenis26.target.rug.nl/downloads/gonl_public/variants/release5/"
-# getting all video links 
+# getting all .gz, .tbi, .md5 download links 
 file_links = get_file_links(archive_url = url)
 
-# download all videos 
-download_files_series(file_links=file_links,file_to_select='chr22')
+# download  all .gz, .tbi, .md5 download links
+filetoselect='chr22'
+download_files_series(file_links=file_links,file_to_select=filetoselect)
 
-#%% analysis
+
+#%% step 2a: load vcf
 
 '''
+Documentation:
+http://alimanfoo.github.io/2017/06/14/read-vcf.html
 https://www.internationalgenome.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-40
 https://pypi.org/project/PyVCF/
+
 * ``Record.CHROM``
 * ``Record.POS``
 * ``Record.ID``
@@ -42,38 +66,79 @@ https://pypi.org/project/PyVCF/
 * ``Record.INFO`
     
 '''
-InputFile="file//"+"gonl.chr22.snps_indels.r5.vcf.gz"
 
-vcf_reader = vcf.Reader(filename=InputFile)
-for record in vcf_reader:
-    print(record)
-    
-vcf_reader = vcf.Reader(filename=InputFile)
-for record in vcf_reader:
-    print(record.POS, record.ID) 
-    
-vcf_reader = vcf.Reader(filename=InputFile)
-for record in vcf_reader:
-    print(record.ID)   
-   
+InputFile="file//"+"gonl."+filetoselect+".snps_indels.r5.vcf.gz" # name of the vcf file
+vcf_reader = vcf.Reader(filename=InputFile) # readthe vcf file
+
+# extract number of observations (number of rows) within vcf file
+rec=[]
+nrow_vcf=len([rec.extend([record.CHROM]) for record in vcf_reader])
+print('\nvcf contains {} rows'.format(nrow_vcf))
+
+# few tests 
+DoTest=False
+if DoTest:
+    for record in vcf_reader:
+        print(record)
+        
+    vcf_reader = vcf.Reader(filename=InputFile)
+    for record in vcf_reader:
+        print(record.POS, record.ID) 
+        
+    vcf_reader = vcf.Reader(filename=InputFile)
+    for record in vcf_reader:
+        print(record.ID)          
     
 
-# %% data base
-from sqlalchemy import create_engine, MetaData
+#%% step 2b: extract data from vcf
+
+# Empty list of values
+values_list = []
+inaccess=0
+
+# Iterate over the rows
+vcf_reader = vcf.Reader(filename=InputFile)
+for record in vcf_reader:
+    # filter the inaccessible records
+    if record.FILTER == ['Inaccessible']:
+        inaccess+=1
+        continue
+    else:
+        # Create a dictionary with the values
+        data = {'chromosome':record.CHROM,
+                'position':record.POS,
+                'alternative':str(record.ALT)[1:-1],
+                'total_number_of_allele': record.INFO['AN'],
+                'variant_frequency': float(str(record.INFO['AF'])[1:-1]),
+                'ID':record.ID,
+                'reference':record.REF}
+        
+    # Append the dictionary to the values list
+    values_list.append(data)
+
+print(inaccess) #16215
+print(len(values_list)) #263278
+print(inaccess+len(values_list)) #279493
+print(values_list[0:1])
+
+# Assessment
+if nrow_vcf == (inaccess+len(values_list)):
+    print('\ninitial n of observations {}\nn of inaccessible removed:{}\nfinal n of observations: {}\n'.format(inaccess+len(values_list),inaccess,len(values_list)))
+else:
+    raise ValueError('sum of removed n of obs and final obs dont equalize initial n of obs in the vcf')
+
+# %% step 3: Instantiate the sqllite data base
 
 # initialize db    
-# Define an engine to connect to chapter5.sqlite: engine
+# Define an engine to connect to chr22.sqlite: engine
 engine = create_engine('sqlite:///chr22.sqlite')
 
 # Initialize MetaData: metadata
 metadata = MetaData()
 
-
-# Import Table, Column, String, and Integer
-from sqlalchemy import Table, Column, String, Integer, Float
-
-# Build a census table: census
+# Build a chr22 table: chr22
 chr22 = Table('chr22', metadata,
+               Column('chromosome', String()),
                Column('position', Integer()),
                Column('alternative', String()),
                Column('total_number_of_allele', Integer()),
@@ -83,60 +148,10 @@ chr22 = Table('chr22', metadata,
 
 # Create the table in the database
 metadata.create_all(engine)
+
 print(engine.table_names())
 
-# populate db
-# Create an empty list: values_list
-#values_list = []
-
-
-## Iterate over the rows
-#vcf_reader = vcf.Reader(filename=InputFile)
-#for record in vcf_reader:
-#    # Create a dictionary with the values
-#    data = {'position':record.POS,
-#            'alternative':str(record.ALT)[1:-1],
-#            'total_number_of_allele': record.INFO['AN'],
-#            'variant_frequency': float(str(record.INFO['AF'])[1:-1]),
-#            'ID':record.ID,
-#            'reference':record.REF}
-#    
-#    # Append the dictionary to the values list
-#    values_list.append(data)
-#
-#print(len(values_list))#279493
-#print(values_list[0:1])
-
-
-values_list = []
-# Iterate over the rows
-inacess=0
-vcf_reader = vcf.Reader(filename=InputFile)
-for record in vcf_reader:
-    if record.FILTER == ['Inaccessible']:
-        inacess+=1
-        continue
-    else:
-        # Create a dictionary with the values
-        data = {'position':record.POS,
-                'alternative':str(record.ALT)[1:-1],
-                'total_number_of_allele': record.INFO['AN'],
-                'variant_frequency': float(str(record.INFO['AF'])[1:-1]),
-                'ID':record.ID,
-                'reference':record.REF}
-        
-        # Append the dictionary to the values list
-    values_list.append(data)
-
-print(inacess)#16215
-print(len(values_list))#263278
-print(inacess+len(values_list))#279493
-print(values_list[0:1])
-
-# Import insert
-from sqlalchemy import insert
-from sqlalchemy import select, func
-#https://stackoverflow.com/questions/9706059/setting-a-default-value-in-sqlalchemy
+# %% step 4: insert to the database
 
 # Build insert statement: stmt
 stmt=insert(chr22)
@@ -146,23 +161,15 @@ connection=engine.connect()
 results=connection.execute(stmt, values_list)
 
 # Print rowcount
-print(results.rowcount)
+print('\nn of inserted values: {}'.format(results.rowcount))
 
-
-
-
-
-
-
-
-from sqlalchemy import select
+# %% step 5a: Check the data in db
 stmt=select([chr22])
-results=connection.execute(stmt).fetchmany(1000)
+results=connection.execute(stmt).fetchmany(100)
 
+# print columns names
 print(chr22.columns.keys())
-for res in results:
-    print(res['position'],res['alternative'], res['total_number_of_allele'], res['variant_frequency'],res['ID'], res['reference'] )
-    
+
 # Print the column names
 print(chr22.columns.keys())
 
@@ -172,96 +179,40 @@ print(repr(chr22))
 for res in results:
     print(res['alternative'], res['total_number_of_allele'], res['variant_frequency'])
 
-
-
-
-
-
-
-
-
-'''
-Conceive an SQL database schema, which will permit you to store the variant data, such
-as genomic position, nucleotide changes, alternative allele count, total number of alleles,
-variant frequency, a unique variant ID (identifier) and the dbSNP reference (rsid).
-
-The data scheme should be optimised for queries based on genomic intervals (e.g. find
-all of the variants present in chromosome 22 between genomic positions n and m).
-Write a program in Python (or another programming language you are more comfortable
-with) to populate this database from the VCF file from GoNL (SNVs and indels). Exclude
-variants "Inaccessible"
-
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import tabix
-import vcfpy
-import sqlalchemy
-
-
-reader = vcfpy.Reader.from_path("file//"+"gonl.chr22.snps_indels.r5.vcf.gz")
-
-i=0
-for rec in reader:
-    print(rec)
-    i+=1
-
-import vcfpy
-# Open file, this will read in the header
-reader = vcfpy.Reader.from_path(InputFile)
-# Build and print header
-header = ['#CHROM', 'POS', 'REF', 'ALT'] + reader.header.samples.namesprint('\t'.join(header))
-
-
-
-
-
-
-
-
-
-
-
-# tabix
-# Open a remote or local file.
-tb = tabix.open("file//"+"gonl.chr22.snps_indels.r5.vcf.gz")
-
-# These queries are identical. A query returns an iterator over the results.
-records = tb.query("19", 1000000, 1250000)
-records = tb.queryi(19, 1000000, 1250000)
-records = tb.querys("1:1000000-1250000")
-
-# Each record is a list of strings.
-for record in reader:
-    print(record)
-
-#vcfpy
-# Open input, add FILTER header, and open output file
-reader = vcfpy.Reader.from_path("file//"+"gonl.chr22.snps_indels.r5.vcf.gz")
-reader.header.add_filter_line(vcfpy.OrderedDict([
-    ('ID', 'DP10'), ('Description', 'total DP < 10')]))
-writer = vcfpy.Writer.from_path('/home/xbouteiller/Downloads/stout', reader.header)
-
-# Add "DP10" filter to records having less than 10 reads
-for record in reader:
-    print(record)
-    ad = sum(c.data.get('DP', 0) for c in record.calls)
-    if ad < 10:
-        record.add_filter('DP10')
-    writer.write_record(record)
+for res in results:
+    print(res['position'],res['alternative'], res['total_number_of_allele'],
+          res['variant_frequency'],res['ID'], res['reference'] )
     
-    help(vcfpy.Reader)
+# %% step 5b: db extraction based on position
+    
+n=16054679 # lower boundary position
+m=16218083 # upper boundary position
+stmt = select([chr22]).where(and_(chr22.columns.position < m, 
+                             chr22.columns.position > n)
+    )
+
+
+results=connection.execute(stmt).fetchall()
+
+# return results within a pandas data frame
+df_extracted = pd.DataFrame(results)
+df_extracted.columns = results[0].keys()
+df_extracted.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
